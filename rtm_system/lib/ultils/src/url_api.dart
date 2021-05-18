cf74d8156cda641038188d0962d630613c77b771
Thanh Tu <lethanhtu18031999@gmail.com>
2021-05-18 23:37:27 +0700

    Demo GetAPI Product

--- rtm_system/lib/ultils/src/url_api.dart
+++ rtm_system/lib/ultils/src/url_api.dart
@@ -1,5 +1,6 @@
 
 //String url_main = 'https://passioemployee.azurewebsites.net';
 String url_main = '3.137.137.156:5000';
-String url_Login = 'api/auth/v1/login';
+String url_login = 'api/auth/v1/login';
+String url_product= 'api/rtm/v1/product/get-product-list';
 //'${url_main}/${url_get_by_date}/$empId?date=$date',
--- rtm_system/lib/view/manager/allNotice_manager.dart
+++ rtm_system/lib/view/manager/allNotice_manager.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:rtm_system/model/getAPI_product.dart';
+import 'package:rtm_system/model/model_product.dart';
 import 'package:rtm_system/ultils/button.dart';
 import 'package:rtm_system/ultils/src/color_ultils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -31,19 +33,65 @@ class _AllNoticeState extends State<AllNotice> {
                 context, "Tạo thông báo", Icon(Icons.notifications_outlined)),
             preferredSize: Size.fromHeight(60.0)),
       ),
-      body: Container(
-        child: FlatButton(
-            onPressed: () async {
-              SharedPreferences prefs = await SharedPreferences.getInstance();
-              prefs.clear();
-              print('Clear data login');
-              Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                  (route) => false);
-            },
-            child: Text("Logout")),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton(
+              onPressed: () async {
+                List<dynamic> dataList = [];
+                List<DataProduct> dataListProduct = [];
+                GetProduct getProduct = GetProduct();
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                print('Click check api');
+
+                // gọi APIProduct và lấy dữ liệu
+                dataList = await getProduct.createLogin(prefs.getString("access_token"));
+                //Parse dữ liệu
+                dataList.forEach((element) {
+                  Map<dynamic, dynamic> data= element;
+                  dataListProduct.add(DataProduct.fromJson(data));
+                  });
+                print("List product: ${dataListProduct.length}");
+                int count = 0;
+                do{
+                  print("Id: ${dataListProduct[count].id} - Name: ${dataListProduct[count].name} - Description: ${dataListProduct[count].description} - Price: ${dataListProduct[count].price}");
+                  count++;
+                }while(count != dataListProduct.length);
+                },
+              child: Text("Check Get API Product")),
+            FlatButton(
+                onPressed: () async {
+                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                  prefs.clear();
+                  print('Clear data login');
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                      (route) => false);
+                },
+                child: Text("Logout")),
+          ],
+        ),
       ),
     );
   }
 }
+
+// List<dynamic> data_list = [];
+// List<DataNews> data_list_news = [];
+// void _getNews() async{
+//   int i = 0;
+//   final prefs = await SharedPreferences.getInstance();
+//   setState((){
+//     token = prefs.getString('token');
+//     print('${prefs.getString('id_emp')}');
+//     // print("Token News: $token");
+//   });
+//   data_list = await getNew.getNews(token);
+//   print('${data_list.length}');
+//   data_list.forEach((element) {
+//     Map<dynamic, dynamic> data= element;
+//     data_list_news.add(DataNews.fromJson(data));
+//   });
