6a646b63df94bc11446b236a0670ed8065984743
trangbttdev <trangbttdev@gmail.com>
2021-05-26 22:17:49 +0700

    Fix flutter_datetime_picker

--- rtm_system/lib/view/customer/advance/all_advance.dart
+++ rtm_system/lib/view/customer/advance/all_advance.dart
@@ -2,9 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:rtm_system/presenter/Customer/show_all_advance.dart';
-import 'package:rtm_system/ultils/button.dart';
 import 'package:rtm_system/ultils/component.dart';
-import 'package:rtm_system/view/customer/invoice/detail_invoice.dart';
 
 class AdvancePage extends StatefulWidget {
   const AdvancePage({Key key}) : super(key: key);
@@ -14,20 +12,20 @@ class AdvancePage extends StatefulWidget {
 }
 
 class _AdvancePageState extends State<AdvancePage> {
-  // var currentDate = new DateTime.now();
-  // var formatter = new DateFormat('dd-MM-yyyy');
-  //
-  // Future<void> _selectDate(BuildContext context) async {
-  //   final DateTime pickedDate = await showDatePicker(
-  //       context: context,
-  //       initialDate: currentDate,
-  //       firstDate: DateTime(2015),
-  //       lastDate: DateTime(2050));
-  //   if (pickedDate != null && pickedDate != currentDate)
-  //     setState(() {
-  //       currentDate = pickedDate;
-  //     });
-  // }
+  var currentDate = new DateTime.now();
+  var formatter = new DateFormat('dd-MM-yyyy');
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime pickedDate = await showDatePicker(
+        context: context,
+        initialDate: currentDate,
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2050));
+    if (pickedDate != null && pickedDate != currentDate)
+      setState(() {
+        currentDate = pickedDate;
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +44,7 @@ class _AdvancePageState extends State<AdvancePage> {
             child: Column(
               children: [
                 headerInvoice('Ứng tiền', 'Tổng số tiền phải trả', '37,000,000 VND'),
-                // _showProcessDate(),
+                _showProcessDate(),
                 SizedBox(
                   height: 12,
                 ),
@@ -58,79 +56,79 @@ class _AdvancePageState extends State<AdvancePage> {
     );
   }
 
-  // Widget _showProcessDate() {
-  //   return Column(
-  //     children: [
-  //       Row(
-  //         mainAxisAlignment: MainAxisAlignment.center,
-  //         children: [
-  //           SizedBox(
-  //             width: 150,
-  //             child: RaisedButton(
-  //               color: Color(0xFFF8D375),
-  //               onPressed: () => {
-  //                 // chuyển đến trang cần xử lý
-  //               },
-  //               child: Row(
-  //                 children: [
-  //                   Column(
-  //                     children: [
-  //                       Icon(Icons.access_time_outlined),
-  //                     ],
-  //                   ),
-  //                   Column(
-  //                     children: [Text('  ')],
-  //                   ),
-  //                   Column(
-  //                     children: [
-  //                       Text(
-  //                         'Chờ xử lý',
-  //                         style: TextStyle(),
-  //                       ),
-  //                     ],
-  //                   ),
-  //                 ],
-  //               ),
-  //               shape: RoundedRectangleBorder(
-  //                 borderRadius: BorderRadius.circular(10.0),
-  //               ),
-  //               elevation: 10,
-  //             ),
-  //           ),
-  //           Text('      '),
-  //           SizedBox(
-  //             width: 150,
-  //             child: RaisedButton(
-  //               onPressed: () => _selectDate(context),
-  //               child: Row(
-  //                 children: [
-  //                   Column(
-  //                     children: [
-  //                       Icon(Icons.calendar_today),
-  //                     ],
-  //                   ),
-  //                   Column(
-  //                     children: [Text('  ')],
-  //                   ),
-  //                   Column(
-  //                     children: [
-  //                       Text(formatter.format(currentDate)),
-  //                     ],
-  //                   ),
-  //                 ],
-  //               ),
-  //               color: Color(0xffEEEEEE),
-  //               shape: RoundedRectangleBorder(
-  //                 borderRadius: BorderRadius.circular(10.0),
-  //               ),
-  //               elevation: 10,
-  //             ),
-  //           ),
-  //         ],
-  //       ),
-  //     ],
-  //   );
-  // }
+  Widget _showProcessDate() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 150,
+              child: RaisedButton(
+                color: Color(0xFFF8D375),
+                onPressed: () => {
+                  // chuyển đến trang cần xử lý
+                },
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.access_time_outlined),
+                      ],
+                    ),
+                    Column(
+                      children: [Text('  ')],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Chờ xử lý',
+                          style: TextStyle(),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 10,
+              ),
+            ),
+            Text('      '),
+            SizedBox(
+              width: 150,
+              child: RaisedButton(
+                onPressed: () => _selectDate(context),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.calendar_today),
+                      ],
+                    ),
+                    Column(
+                      children: [Text('  ')],
+                    ),
+                    Column(
+                      children: [
+                        Text(formatter.format(currentDate)),
+                      ],
+                    ),
+                  ],
+                ),
+                color: Color(0xffEEEEEE),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 10,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 
   Widget _showBottomButton() {
     return Column(
--- rtm_system/lib/view/customer/invoice/all_invoice.dart
+++ rtm_system/lib/view/customer/invoice/all_invoice.dart
@@ -12,20 +12,20 @@ class InvoicePage extends StatefulWidget {
 }
 
 class _InvoicePageState extends State<InvoicePage> {
-  // var currentDate = new DateTime.now();
-  // var formatter = new DateFormat('dd-MM-yyyy');
-  //
-  // Future<void> _selectDate(BuildContext context) async {
-  //   final DateTime pickedDate = await showDatePicker(
-  //       context: context,
-  //       initialDate: currentDate,
-  //       firstDate: DateTime(2015),
-  //       lastDate: DateTime(2050));
-  //   if (pickedDate != null && pickedDate != currentDate)
-  //     setState(() {
-  //       currentDate = pickedDate;
-  //     });
-  // }
+  var currentDate = new DateTime.now();
+  var formatter = new DateFormat('dd-MM-yyyy');
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime pickedDate = await showDatePicker(
+        context: context,
+        initialDate: currentDate,
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2050));
+    if (pickedDate != null && pickedDate != currentDate)
+      setState(() {
+        currentDate = pickedDate;
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +43,7 @@ class _InvoicePageState extends State<InvoicePage> {
             margin: EdgeInsets.only(top: 12, ),
             child: Column(
               children: [
-                // _showProcessDate(),
+                _showProcessDate(),
                 SizedBox(
                   height: 12,
                 ),
@@ -110,77 +110,77 @@ class _InvoicePageState extends State<InvoicePage> {
     ));
   }
 
-  // Widget _showProcessDate() {
-  //   return Column(
-  //     children: [
-  //       Row(
-  //         mainAxisAlignment: MainAxisAlignment.center,          children: [
-  //           SizedBox(
-  //             width: 150,
-  //             child: RaisedButton(
-  //               color: Color(0xFFF8D375),
-  //               onPressed: () => {
-  //                 // chuyển đến trang cần xử lý
-  //               },
-  //               child: Row(
-  //                 children: [
-  //                   Column(
-  //                     children: [
-  //                       Icon(Icons.access_time_outlined),
-  //                     ],
-  //                   ),
-  //                   Column(
-  //                     children: [Text('  ')],
-  //                   ),
-  //                   Column(
-  //                     children: [
-  //                       Text('Chờ xử lý', style: TextStyle(
-  //
-  //                       ),),
-  //                     ],
-  //                   ),
-  //                 ],
-  //               ),
-  //               shape: RoundedRectangleBorder(
-  //                 borderRadius: BorderRadius.circular(10.0),
-  //               ),
-  //               elevation: 10,
-  //             ),
-  //           ),
-  //           Text('      '),
-  //           SizedBox(
-  //             width: 150,
-  //             child: RaisedButton(
-  //               onPressed: () => _selectDate(context),
-  //               child: Row(
-  //                 children: [
-  //                   Column(
-  //                     children: [
-  //                       Icon(Icons.calendar_today),
-  //                     ],
-  //                   ),
-  //                   Column(
-  //                     children: [Text('  ')],
-  //                   ),
-  //                   Column(
-  //                     children: [
-  //                       Text(formatter.format(currentDate)),
-  //                     ],
-  //                   ),
-  //                 ],
-  //               ),
-  //               color: Color(0xffEEEEEE),
-  //               shape: RoundedRectangleBorder(
-  //                 borderRadius: BorderRadius.circular(10.0),
-  //               ),
-  //               elevation: 10,
-  //             ),
-  //           ),
-  //         ],
-  //       ),
-  //     ],
-  //   );
-  // }
+  Widget _showProcessDate() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,          children: [
+            SizedBox(
+              width: 150,
+              child: RaisedButton(
+                color: Color(0xFFF8D375),
+                onPressed: () => {
+                  // chuyển đến trang cần xử lý
+                },
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.access_time_outlined),
+                      ],
+                    ),
+                    Column(
+                      children: [Text('  ')],
+                    ),
+                    Column(
+                      children: [
+                        Text('Chờ xử lý', style: TextStyle(
+
+                        ),),
+                      ],
+                    ),
+                  ],
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 10,
+              ),
+            ),
+            Text('      '),
+            SizedBox(
+              width: 150,
+              child: RaisedButton(
+                onPressed: () => _selectDate(context),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.calendar_today),
+                      ],
+                    ),
+                    Column(
+                      children: [Text('  ')],
+                    ),
+                    Column(
+                      children: [
+                        Text(formatter.format(currentDate)),
+                      ],
+                    ),
+                  ],
+                ),
+                color: Color(0xffEEEEEE),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 10,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 
   Widget _showBottomButton() {
     return Column(
