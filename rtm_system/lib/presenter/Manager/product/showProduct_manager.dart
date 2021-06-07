248d46aa1c393b229d29a77e0d02a7d52f9ec9b7
Thanh Tu <lethanhtu18031999@gmail.com>
2021-06-07 12:19:59 +0700

    .

--- rtm_system/lib/presenter/Manager/product/showProduct_manager.dart
+++ rtm_system/lib/presenter/Manager/product/showProduct_manager.dart
@@ -18,56 +18,21 @@ class showAllProduct extends StatefulWidget {
 
 class _showAllProductState extends State<showAllProduct> {
   // String token;
-  // List<DataProduct> dataListProduct = [];
+  List<DataProduct> dataListProduct = [];
   List<dynamic> dataList = [];
-  final PagingController _pagingController = PagingController(firstPageKey: 10);
-  // Future _getToken() async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   setState(() {
-  //     token = prefs.getString("access_token");
-  //   });
-  // }
-  //
-  // Future _getProduct() async {
-  //   List<dynamic> dataList = [];
-  //   GetProduct getProduct = GetProduct();
-  //   //Nếu dùng hàm này thì FutureBuilder sẽ chạy vòng lập vô hạn
-  //   //Phải gọi _getToken trước khi gọi hàm _getProduct
-  //   // await _getToken();
-  //   // gọi APIProduct và lấy dữ liệu
-  //
-  //   //Khi click nhiều lần vào button "Sản phẩm" thì sẽ có hiện tượng dữ liệu bị ghi đè
-  //   //Clear là để xoá dữ liệu cũ, ghi lại dữ liệu mới
-  //   dataListProduct.clear();
-  //
-  //   //Nếu ko có If khi FutureBuilder gọi hàm _getProduct lần đầu thì Token chưa trả về nên sẽ bằng null
-  //   //FutureBuilder sẽ gọi đến khi nào có giá trị trả về
-  //   //Ở lần gọi thứ 2 thì token mới có giá trị
-  //   if (token.isNotEmpty) {
-  //     print(token);
-  //     dataList = await getProduct.getProduct(token, 0);
-  //     //Parse dữ liệu
-  //     dataList.forEach((element) {
-  //       Map<dynamic, dynamic> data = element;
-  //       dataListProduct.add(DataProduct.fromJson(data));
-  //     });
-  //
-  //     return dataListProduct;
-  //   }
-  // }
+  final PagingController _pagingController = PagingController(firstPageKey: 50);
 
   Future<void> _fetchPage(pageKey) async {
     try {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       GetProduct getProduct = GetProduct();
-      dataList = await getProduct.getProduct(prefs.getString("access_token"), 0);
-
+      dataList =
+          await getProduct.getProduct(prefs.getString("access_token"), 0);
       // print("${_pagingController}");
       final isLastPage = dataList.length < pageKey;
       if (isLastPage) {
         _pagingController.appendLastPage(dataList);
       } else {
-
         final nextPageKey = pageKey;
         _pagingController.appendPage(dataList, nextPageKey);
       }
@@ -110,40 +75,6 @@ class _showAllProductState extends State<showAllProduct> {
       margin: EdgeInsets.only(top: 10, left: 20, right: 20),
       height: size.height,
       width: size.width,
-      // child: new FutureBuilder(
-      //   future: _getProduct(),
-      //   builder: (BuildContext context, AsyncSnapshot snapshot) {
-      //     if (snapshot.hasData) {
-      //       return ListView.builder(
-      //         itemCount: snapshot.data.length,
-      //         itemBuilder: (context, index) {
-      //           return card(
-      //               context,
-      //               snapshot.data[index].name,
-      //               "Loại",
-      //               "${snapshot.data[index].type}",
-      //               snapshot.data[index].price,
-      //               snapshot.data[index].date_time,
-      //               Colors.black54,
-      //               FormForDetailPage(
-      //                 tittle: "Chi tiết sản phẩm",
-      //                   bodyPage: DetailProduct(
-      //                 id: snapshot.data[index].id,
-      //                 name: snapshot.data[index].name,
-      //                 description: snapshot.data[index].description,
-      //                 type: snapshot.data[index].type,
-      //                 date_time: snapshot.data[index].date_time,
-      //                 price: snapshot.data[index].price,
-      //               )));
-      //         },
-      //       );
-      //     }
-      //     return Container(
-      //         height: size.height * 0.7,
-      //         child: Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(welcome_color),)));
-      //   },
-      // ),
-
       child: new CustomScrollView(
         slivers: <Widget>[
           PagedSliverList(
@@ -164,11 +95,33 @@ class _showAllProductState extends State<showAllProduct> {
                     ],
                   );
                 },
-                firstPageProgressIndicatorBuilder: (context) => firstPageProgressIndicatorBuilder(),
-                noItemsFoundIndicatorBuilder: (context) => noItemsFoundIndicatorBuilder(),
-                newPageProgressIndicatorBuilder: (context) => firstPageProgressIndicatorBuilder(),
+                firstPageProgressIndicatorBuilder: (context) =>
+                    firstPageProgressIndicatorBuilder(),
+                noItemsFoundIndicatorBuilder: (context) =>
+                    noItemsFoundIndicatorBuilder(),
+                newPageProgressIndicatorBuilder: (context) =>
+                    firstPageProgressIndicatorBuilder(),
                 itemBuilder: (context, item, index) {
-
+                  return card(
+                      context,
+                      item["name"],
+                      "Loại",
+                      "${item["type"]}",
+                      "${item["update_price"]}",
+                      item["updateDateTime"],
+                      Colors.black54,
+                      FormForDetailPage(
+                          tittle: "Chi tiết sản phẩm",
+                          bodyPage: DetailProduct(
+                            itemDetailProduct: item,
+                            // id: item["id"],
+                            // name: item["name"],
+                            // description: item["description"],
+                            // type: "${item["type"]}",
+                            // date_time: item["updateDateTime"],
+                            // price: "${item["update_price"]}",
+                          ))
+                  );
                 }),
           ),
         ],
@@ -182,7 +135,7 @@ class _showAllProductState extends State<showAllProduct> {
     super.dispose();
   }
 
-  Widget noItemsFoundIndicatorBuilder(){
+  Widget noItemsFoundIndicatorBuilder() {
     return Column(
       children: [
         firstPageErrorIndicatorBuilder(context,
