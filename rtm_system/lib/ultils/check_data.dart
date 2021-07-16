fe3161e3ec0d00ced0fbf38f3d8923e1f0e9f259
trangbttdev <trangbttdev@gmail.com>
2021-07-16 10:19:01 +0700

    Fix show message

--- rtm_system/lib/ultils/check_data.dart
+++ rtm_system/lib/ultils/check_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rtm_system/helpers/dialog.dart';
+import 'package:rtm_system/ultils/src/message_list.dart';
 
 import 'src/regExp.dart';
 
@@ -7,18 +8,18 @@ checkInputUpdateInvoice(BuildContext context,
     {bool isDegree, double quantity, double degree}) {
   if (isDegree) {
     if (quantity == 0 && degree == 0) {
-      showCupertinoAlertDialog(context, "Chưa điền khối lượng, số độ");
+      showCupertinoAlertDialog(context, showMessage("Khối lượng, số độ", MSG001));
       return false;
     } else if (quantity == 0) {
-      showCupertinoAlertDialog(context, "Chưa điền khối lượng");
+      showCupertinoAlertDialog(context, showMessage("Khối lượng", MSG001));
       return false;
     } else if (degree == 0) {
-      showCupertinoAlertDialog(context, "Chưa điền số độ");
+      showCupertinoAlertDialog(context, showMessage("Số độ", MSG001));
       return false;
     }
   } else {
     if (quantity == 0) {
-      showCupertinoAlertDialog(context, "Chưa điền khối lượng");
+      showCupertinoAlertDialog(context, showMessage("Khối lượng", MSG001));
       return false;
     }
   }
@@ -32,25 +33,25 @@ checkUpdatePriceProduct(BuildContext context, {bool isClick, double price}) {
         return true;
       } else {
         showCustomDialog(context,
-            content: "Giá phải lớn hơn 1000đ", isSuccess: false);
+            content: showMessage("", MSG035), isSuccess: false);
         return false;
       }
     } else {
-      showCustomDialog(context, content: "Giá đang trống", isSuccess: false);
+      showCustomDialog(context, content: showMessage("Giá", MSG001), isSuccess: false);
       return false;
     }
   } else {
-    showCustomDialog(context, content: "Hãy chọn sản phẩm", isSuccess: false);
+    showCustomDialog(context, content: showMessage("", MSG013), isSuccess: false);
     return false;
   }
 }
 
 checkPhoneNumber(String phone) {
   if (phone == null || phone == "") {
-    return "Số điện thoại trống";
+    return showMessage("Số điện thoại", MSG001);
   } else {
     if (!checkFormatPhone.hasMatch(phone) || phone.length > 11) {
-      return "Số điện thoại sai (10-11 só)";
+      return showMessage("", MSG014);
     } else {
       return null;
     }
@@ -59,20 +60,20 @@ checkPhoneNumber(String phone) {
 
 checkFullName(BuildContext context, String name) {
   if (name == null || name == "") {
-    return "Tên khách hàng trống";
+    return showMessage("Tên khách hàng", MSG001);
   }
   return null;
 }
 
 checkChooseProduct(BuildContext context, String product) {
   if (product == null) {
-    showCustomDialog(context, content: "Chưa chọn sản phẩm", isSuccess: false);
+    showCustomDialog(context, content: showMessage("", MSG013), isSuccess: false);
   }
 }
 
 checkQuantity(double quantity) {
   if (quantity == 0) {
-    return "Số ký đang trống";
+    return showMessage("Số ký", MSG001);
   }
   return null;
 }
@@ -81,7 +82,7 @@ checkDegree(bool checkProduct, double degree) {
   print(degree);
   if (!checkProduct) {
     if (degree == 0) {
-      return "Số độ trống";
+      return showMessage("Số độ", MSG001);
     }
   }
   return null;
@@ -94,31 +95,31 @@ checkPassword(String password, int type, {String passwordCheck}) {
   if (password == null || password == "") {
     switch (type) {
       case 0:
-        return "Mật khẩu trống";
+        return showMessage("Mật khẩu", MSG001);
         break;
       case 1:
-        return "Mật khẩu mới trống";
+        return showMessage("Mật khẩu mới", MSG001);
         break;
       case 2:
-        return "Mật khẩu xác nhận trống";
+        return showMessage("Mật khẩu xác nhận", MSG001);
         break;
     }
   } else
     switch (type) {
       case 0:
         if (passwordCheck != password) {
-          return "Mật khẩu sai";
+          return showMessage("Mật khẩu", MSG020);
         } else
           return null;
         break;
       case 1:
         if (!checkFormatPassword.hasMatch(password)) {
-          return "Mật khẩu ít nhất 6 kí tự (chữ và số)";
+          return showMessage("", MSG016);
         } else return null;
           break;
       case 2:
         if (password != passwordCheck) {
-          return "Mật khẩu xác nhận không khớp";
+          return showMessage("", MSG021);
         } else return null;
         break;
     }
