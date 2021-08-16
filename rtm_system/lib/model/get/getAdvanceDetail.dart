e95ad104c388d70ecb24826491d837aae751a4a5
Thanh Tu <lethanhtu18031999@gmail.com>
2021-08-16 14:59:25 +0700

    Fix bug Dialog Update Price

--- rtm_system/lib/model/get/getAdvanceDetail.dart
+++ rtm_system/lib/model/get/getAdvanceDetail.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:rtm_system/model/model_AdvanceDetail.dart';
+import 'package:rtm_system/ultils/check_data.dart';
 import 'package:rtm_system/ultils/src/url_api.dart';
 import 'package:http/http.dart' as http;
 
 class GetAdvanceDetail{
   static int statusInvoice;
-  getAdvanceDetail(String token, String id, int statusId) async {
+  getAdvanceDetail(BuildContext context,String token, String id, int statusId) async {
     final response = await http.get(
     Uri.http('$urlMain', '$urlAdvanceDetail/$id', { "status_id" : "$statusId"}),
       headers: <String, String>{
@@ -20,6 +22,7 @@ class GetAdvanceDetail{
       return  AdvanceDetail.fromJson(jsonDecode(response.body));
     } else {
       // throw an exception.
+      checkTimeToken(context, response.statusCode);
       throw Exception('Failed to load data');
     }
   }
